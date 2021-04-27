90c2409b16e6b3115fc02be59df3c60841031691
Ringo Hoffmann <riho@live.de>
2021-04-27 21:18:27 +0200

    parse line breaks in putput [fix #1]

--- src/components/ResultViewer.tsx
+++ src/components/ResultViewer.tsx
@@ -11,17 +11,29 @@ function ResultViewer(props: ResultViewerProps) {
       {props.res.stdout && (
         <div>
           <div className="heading">stdout</div>
-          <div>{props.res.stdout}</div>
+          <div>{linebreak(props.res.stdout)}</div>
         </div>
       )}
       {props.res.stderr && (
         <div>
           <div className="heading">stderr</div>
-          <div className="stderr">{props.res.stderr}</div>
+          <div className="stderr">{linebreak(props.res.stderr)}</div>
         </div>
       )}
     </div>
   );
+}
+
+function linebreak(s: string): JSX.Element[] {
+  const split = s.trim().split('\n');
+  const res = split.slice(0, split.length - 1).map((l) => (
+    <span>
+      {l}
+      <br />
+    </span>
+  ));
+  res.push(<span>{split[split.length - 1]}</span>);
+  return res;
 }
 
 export default ResultViewer;
